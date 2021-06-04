be9908655faeead29c3c03a8172d11b296c4a3b6
RyanSu <563607356@qq.com>
2021-06-04 12:56:26 +0800

    增加了github地址

---
+++ assets/introduce.fea4368a.js
@@ -0,0 +1,1 @@
+export default'<h3 id="项目介绍">项目介绍</h3>\n<p>本项目是为了学习vue3.0而写的一个组件库，使用<code>Vue3</code>,<code>typescript</code>、<code>vite</code>可以让你快速学习vue3的新特性。</p>\n<h3 id="重要概念">重要概念</h3>\n<pre><code>1. 数据响应式\n2. 组件化\n3. 编译\n4. 事件、双向绑定、插槽\n5. 缓存组件、动画（transition）\n6. Vue-Router\n9. Vuex\n</code></pre>\n';
